2ef9e4a7c9e12ab8ecd68fa2a58629bd75cbc39e
arilmar <arrarez@gmail.com>
2018-12-03 10:01:24 -0200

    Sem erros de bibliotecas

--- src/conexao/SingletonConnetion.java
+++ src/conexao/SingletonConnetion.java
@@ -1,22 +1,19 @@
 package conexao;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 
-import org.primefaces.model.diagram.Connection;
-
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
-
 /**
- * Classe responsável por controlar , criar e retornar a conexão com o banco de dados
+ * Classe responsï¿½vel por controlar , criar e retornar a conexï¿½o com o banco de dados
  * @author alex
  *
  */
 public class SingletonConnetion {
 
-	//Conexão SQL Java
+	//Conexï¿½o SQL Java
 	private static Connection connection = null;
 	
-	// Método estático que faz sempre a chamada para conectar
+	// Mï¿½todo estï¿½tico que faz sempre a chamada para conectar
 	static {
 		conectar();
 	}
@@ -26,39 +23,39 @@ public class SingletonConnetion {
 	}
 
 	/**
-	 * Realiza a conexão com o banco de dados
+	 * Realiza a conexï¿½o com o banco de dados
 	 */
 	private static void conectar() {
 		try {
 			
 			if (connection == null) {
-				/* Carrega o driver especificado para conexão ao BD*/
+				/* Carrega o driver especificado para conexï¿½o ao BD*/
 				Class.forName("com.mysql.cj.jdbc.Driver");	
 /*				Class.forName("org.postgresql.Driver"); // driver mysql
 				connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/projeto_imagens?autoReconnect=true", "postgres", "admin");// url do banco de dados com user e senha
-				connection.setAutoCommit(false);// não dar commit automatico
-				connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // tipo da transação no banco de dados
+				connection.setAutoCommit(false);// nï¿½o dar commit automatico
+				connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // tipo da transaï¿½ï¿½o no banco de dados
 */
 
-				//URL de conexão ao BD
+				//URL de conexï¿½o ao BD
 				String url = "jdbc:mysql://127.0.0.1:3306/projeto_imagens?useTimezone=true&serverTimezone=UTC&autoReconnect=true&useSSL=false";
-				//Usuário usado para conectar ao BD
+				//Usuï¿½rio usado para conectar ao BD
 				String user = "root";
-				//Senha do usuário a ser usado na conexão
+				//Senha do usuï¿½rio a ser usado na conexï¿½o
 				String pass =  "Transcon2018";
 				
 				/* 
-				*	getConnection ==> abre a conexão
-				*	A interface Connection ==>	Representa uma conexão ao banco de dados
-				*	Método close ==>	Geralmente é inserido dentro do bloco finally para realizar sempre a sua execução, pois esse método serve para fechar e liberar imediatamente um objeto Connection.
-				*	Método isClosed ==>	Serve para verificar se o objeto Connection está fechado.
-				*	Método createStatement ==>	É usado para criar um objeto Statement que executa instruções SQL ao banco de dados.
+				*	getConnection ==> abre a conexï¿½o
+				*	A interface Connection ==>	Representa uma conexï¿½o ao banco de dados
+				*	Mï¿½todo close ==>	Geralmente ï¿½ inserido dentro do bloco finally para realizar sempre a sua execuï¿½ï¿½o, pois esse mï¿½todo serve para fechar e liberar imediatamente um objeto Connection.
+				*	Mï¿½todo isClosed ==>	Serve para verificar se o objeto Connection estï¿½ fechado.
+				*	Mï¿½todo createStatement ==>	ï¿½ usado para criar um objeto Statement que executa instruï¿½ï¿½es SQL ao banco de dados.
 				*/
 				
 				connection = DriverManager.getConnection(url,user,pass);
 				
-				connection.setAutoCommit(false);// não dar commit automatico
-				//connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // tipo da transação no banco de dados
+				connection.setAutoCommit(false);// nï¿½o dar commit automatico
+				//connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // tipo da transaï¿½ï¿½o no banco de dados
 				
 				}
 		} catch (Exception e) {
@@ -69,7 +66,7 @@ public class SingletonConnetion {
 	}
 
 	/**
-	 * Retorna a conexão com o banco de dados
+	 * Retorna a conexï¿½o com o banco de dados
 	 * @return Connection
 	 */
 	public static Connection getConnection() {
